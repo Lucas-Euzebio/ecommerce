d4ba2ce690a6323d4ba21cd5c34bcc002457ccee
Lucas-Euzebio <lucas.eletrica.uftm@gmail.com>
2023-04-17 18:01:57 -0300

    ctrl+c ctrl+v initial project

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/Lucas-Euzebio/ecommerce/controllers"
+	"github.com/Lucas-Euzebio/ecommerce/database"
+	"github.com/Lucas-Euzebio/ecommerce/middleware"
+	"github.com/Lucas-Euzebio/ecommerce/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.newApplication(database.ProductDta(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UseRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+
+}
